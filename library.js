7ed9edcb230fd02787f6d9eb46917eb090f7c103
s-darren <295744570@qq.com>
2020-11-09 15:10:17 +0800

    调整获取月份函数传参，月份传参为1-12，直观一点

--- library.js
+++ library.js
@@ -83,8 +83,9 @@ function isLeapYear(year) {
  * @param {Number} year
  * @param {Number} month
  */
-function getMonthDays(year, month) {
-  return [31, null, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month] || (isLeapYear(year) ? 29 : 28);
+function getMonthDays(year = new Date().getFullYear(), month = new Date().getMonth() + 1) {
+  let MonthDay = [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month - 1]
+  return MonthDay || 0;
 }
 
 /**
@@ -101,7 +102,7 @@ function getWeekNumber(y, m, d) {
   let days = now.getDate()
   //那一天是那一年中的第多少天
   for (var i = 0; i < month; i++) {
-    days += getMonthDays(year, i);
+    days += getMonthDays(year, i + 1);
   }
 
   //那一年第一天是星期几
@@ -130,7 +131,7 @@ function getMonthWeek(y) {
     let monthWeeks = []
     let firstday = new Date(y, i, 1)
     let firstweek = getWeekNumber(firstday.getFullYear(), firstday.getMonth() + 1, firstday.getDate())
-    let lastday = new Date(y, i, getMonthDays(y, i))
+    let lastday = new Date(y, i, getMonthDays(y, i + 1))
     let lastweek = getWeekNumber(lastday.getFullYear(), lastday.getMonth() + 1, lastday.getDate())
     for (let j = firstweek; j <= lastweek; j++) {
       monthWeeks.push(j)
