b74b714c5d28e906ce5a0861edeac393a05140ab
s-darren <295744570@qq.com>
2020-11-09 10:20:19 +0800

    依赖更新

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-  plugins: [['transform-vue-jsx'], ["@babel/plugin-proposal-decorators", { "legacy": true }],['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }], ['@babel/plugin-proposal-optional-chaining']]
+  plugins: [
+    ['transform-vue-jsx'],
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
+    ['@babel/plugin-proposal-optional-chaining'],
+    ["@babel/plugin-transform-runtime", { "helpers": false, "useESModules": false }]
+  ]
 };
